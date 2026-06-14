67df9e53d831de1a4be8d9fb770fa9fd405a5b26
Syeda Umaima <syeda.umaima1006@gmail.com>
2026-06-14 12:48:48 +0530

    Refactor Firebase admin credential initialization

--- index.js
+++ index.js
@@ -20,11 +20,8 @@ const client = twilio(
 );
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-  })
+  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_KEY))
+})
 });
 
 const db = admin.firestore();
